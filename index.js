c64bb28ee4275f648609804b4b96e6a322abcaf8
John <theminor@gmail.com>
2021-03-21 11:41:49 -0400

    tidy up code

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 
-const SCOPES = [
+const Scopes = [
 	'https://mail.google.com/',
 	'https://www.googleapis.com/auth/gmail.modify',
 	'https://www.googleapis.com/auth/gmail.compose',
 	'https://www.googleapis.com/auth/gmail.send'
 ];
 
-const TOKEN_PATH = 'token.json';  // The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
+const TokenPath = 'token.json';  // The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
 
 /**
  * Create an OAuth2 client with the given credentials
@@ -23,7 +23,7 @@ function authorize(credentials) {
 			const {client_secret, client_id, redirect_uris} = credentials.installed;
 			oAuth2Client = new google.auth.OAuth2( client_id, client_secret, redirect_uris[0] );
 		} catch (err) { reject(`Gmail send - error with credentials: ${err}`); }
-		fs.readFile(TOKEN_PATH, (err, token) => {  // Check if we have previously stored a token.
+		fs.readFile(TokenPath, (err, token) => {  // Check if we have previously stored a token.
 			if (err) resolve(await getNewToken(oAuth2Client));
 			oAuth2Client.setCredentials(JSON.parse(token));
 			resolve(oAuth2Client);
@@ -42,7 +42,7 @@ function getNewToken(oAuth2Client) {
 		try {
 			const authUrl = oAuth2Client.generateAuthUrl({
 				access_type: 'offline',
-				scope: SCOPES
+				scope: Scopes
 			});
 			console.log('Authorize this app by visiting this url:', authUrl);
 			rl = readline.createInterface({
@@ -56,9 +56,9 @@ function getNewToken(oAuth2Client) {
 				if (err) reject('Gmail send - Error retrieving access token', err);
 				else {
 					oAuth2Client.setCredentials(token);
-					fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {  // Store the token to disk for later program executions
+					fs.writeFile(TokenPath, JSON.stringify(token), (err) => {  // Store the token to disk for later program executions
 						if (err) reject(`Gmail send - error writing token to file: ${err}`);
-						else console.info(`Gmail send - Token stored to ${TOKEN_PATH}`);
+						else console.info(`Gmail send - Token stored to ${TokenPath}`);
 						resolve(oAuth2Client);
 					});
 				}
@@ -114,8 +114,7 @@ function sendMessage(auth, to, from, subject, message) {
  */
 module.exports = async function (credentials, to, from, subject, message, errHandler) {
 	try {
-		let oAuth = await authorize(credentials);
-		return await sendMessage(oAuth, to, from, subject, message);
+		return await sendMessage(await authorize(credentials), to, from, subject, message);
 	} catch (err) {
 		if (errHandler) errHandler(err);
 		else console.error(`Error sending gmail message: ${err}`);
